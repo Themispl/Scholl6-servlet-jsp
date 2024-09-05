814a4b53b5ce424e9ba5c7da0133283d988257c0
Themispl <themis.pilarinos@gmail.com>
2024-09-05 19:01:47 +0300

    UserDAOImpl

--- src/main/java/gr/aueb/cf/schoolapp/dao/UserDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/UserDAOImpl.java
@@ -8,6 +8,7 @@ import gr.aueb.cf.schoolapp.service.util.DBUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAOImpl implements IUserDAO{
@@ -29,22 +30,76 @@ public class UserDAOImpl implements IUserDAO{
 
         }catch (SQLException e) {
             e.printStackTrace();
-            throw new UserDAOException("SQL error in teacher insert" + user);
+            throw new UserDAOException("SQL error in user insert" + user);
         }
     }
 
     @Override
     public User getByUsername(String username) throws UserDAOException {
-        return null;
+        String sql = "SELECT * FROM users where username = ?";
+        User user = null;
+        ResultSet rs;
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps= connection.prepareStatement(sql)){
+            //extract model info
+
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"));
+            }
+            return user;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new UserDAOException("SQL error in user search" + username);
+        }
     }
 
     @Override
     public boolean isUserValid(String username, String password) throws UserDAOException {
-        return false;
+        String sql = "SELECT * FROM users WHERE username = ?";
+        User user = null;
+        ResultSet rs;
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps= connection.prepareStatement(sql)){
+            //extract model info
+
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"));
+            }else{
+                return false;
+            }
+            return SecUtil.checkPassword(password, user.getPassword());
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new UserDAOException("SQL error in user valid for username: " + username);
+        }
     }
 
     @Override
     public boolean isEmailExist(String username) throws UserDAOException {
-        return false;
+        String sql = "SELECT count(*) FROM users WHERE username = ?";
+        User user = null;
+        ResultSet rs;
+        int count = 0;
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps= connection.prepareStatement(sql)){
+            //extract model info
+
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+               count = rs.getInt(1);
+            }
+            return count > 0;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new UserDAOException("SQL error in user valid for email: " + username);
+        }
     }
 }
