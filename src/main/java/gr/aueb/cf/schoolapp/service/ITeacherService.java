ea99975432d6f34ee353206c3684675bcf2c16dd
Themispl <themis.pilarinos@gmail.com>
2024-09-05 19:22:44 +0300

    service exceptions

--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -1,7 +1,11 @@
 package gr.aueb.cf.schoolapp.service;
 
+import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
 
 public interface ITeacherService {
-    //Teacher insertTeacher();
+    Teacher insertTeacher(TeacherInsertDTO dto) throws TeacherDAOException;
+    Teacher updateTeacher(TeacherUpdateDTO dto) throws TeacherDAOException;
 }
